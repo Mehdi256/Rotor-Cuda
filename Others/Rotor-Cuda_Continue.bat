6b814475a55661d5555d3b1ef38414070f30a7fa
LostCoins <82582647+phrutis@users.noreply.github.com>
2021-11-11 20:29:26 +0300

    Delete Rotor-Cuda_Continue.bat

--- Others/Rotor-Cuda_Continue.bat
+++
@@ -1,7 +0,0 @@
-:loop 
-Rotor-Cuda.exe -g --gpui 0 --gpux 256,256 -m xpoint --coin BTC --range 8000000000:ffffffffff a2efa402fd5268400c77c20e574ba86409ededee7c4020e4b9f0edbee53de0d4 -n 2 
-goto :loop 
-created: Tue Nov  2 22:42:45 2021
-214077276160
-To continue correctly, DO NOT change the parameters in this file! 
-If you no longer need the continuation, DELETE this file! 
