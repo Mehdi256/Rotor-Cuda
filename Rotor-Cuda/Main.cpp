dd1c9dcbefa3973e8fad382553c8fedb02a855dc
Han Liang Wee Eric <Eric.aka.darth_vader@hotmail.com>
2022-09-26 01:56:50 +0800

    Added python endings

--- Rotor-Cuda/Main.cpp
+++ Rotor-Cuda/Main.cpp
@@ -642,3 +642,15 @@ int main(int argc, char** argv)
 	return 0;
 #endif
 }
+
+#include <pybind11/pybind11.h>
+
+int add(int i, int j) {
+    return i + j;
+}
+
+PYBIND11_MODULE(rotor, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
+
+    m.def("add", &add, "A function that adds two numbers");
+}
