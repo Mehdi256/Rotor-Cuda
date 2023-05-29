27f7e1f2c8dc56eb32c0f4de83663e18c0b41162
Mehdi <79555777+Mehdi256@users.noreply.github.com>
2023-05-30 00:40:51 +0330

    Delete Rotor-Cuda directory

--- Rotor-Cuda/debug_bloom.sh
+++
@@ -1,6 +0,0 @@
-rm obj/GPU/GPUEngine.o; make gpu=1 CCAP=61 all
-# rm -rf Found.txt; ./Rotor -g --gpui 0 --gpux 1,1 -m addresses --coin eth --range 0:800 --in tc.bin  > Analysisgpu.txt; rm -rf Found.txt; ./Rotor -t 1 -m addresses --coin eth --range 0:800 --in tc.bin  > Analysiscpu.txt
-rm -rf Found.txt; 
-./Rotor -g --gpui 0 --gpux 1,1 -m addresses --coin eth --range 0:800 --in eth-sep.bin  > Analysisgpu.txt;
-# rm -rf Found.txt; 
-# ./Rotor -t 1 -m addresses --coin eth --range 0:800 --in eth-sep.bin > Analysiscpu.txt
