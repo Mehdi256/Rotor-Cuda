c9d945846bf5dc7e24424b373b20f4e547e7060d
Mehdi <79555777+Mehdi256@users.noreply.github.com>
2023-06-15 02:39:31 +0330

    Update GPUBase58.h
    Signed-off-by: Mehdi <79555777+Mehdi256@users.noreply.github.com>

--- Rotor-Cuda/GPU/GPUBase58.h
+++ Rotor-Cuda/GPU/GPUBase58.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of the VanitySearch distribution (https://github.com/JeanLucPons/VanitySearch).
- * Copyright (c) 2019 Jean Luc PONS.
+ * Copyright (c) 2019 Jean Luc PONS..
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
