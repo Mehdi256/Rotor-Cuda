f2a0a03fac3c58db2e4028b8ff7f0e9ad5a3e93d
Mehdi <79555777+Mehdi256@users.noreply.github.com>
2025-09-19 15:58:51 +0330

    Delete x64 directory
    Signed-off-by: Mehdi <79555777+Mehdi256@users.noreply.github.com>

--- x64/Rotor-Cuda_START.bat
+++
@@ -1,4 +0,0 @@
-:loop 
-Rotor-Cuda.exe -g --gpui 0 --gpux 256,256 -m addresses --coin BTC --range 74fa5327cc0f4e947789dd5e989a61a8242986a596f170640ac90337b1da1ee4:751c490aeade62d9f2c4e026e79d76d1c1a7eb8b1902a82ccac90337b1da1ed4 -i .\basee160.bin -n 7 
-goto :loop 
-
